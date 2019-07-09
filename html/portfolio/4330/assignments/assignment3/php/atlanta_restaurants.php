3275b6c30c3535415e9af81abeaf973848bb75f3
bryan.nonni <bryanwnonni@gmail.com>
2019-07-09 16:23:19 -0400

    commiting local changes

--- html/portfolio/4330/assignments/assignment3/php/atlanta_restaurants.php
+++ html/portfolio/4330/assignments/assignment3/php/atlanta_restaurants.php
@@ -45,7 +45,8 @@
   <table class="restaurants" width="300px" cellspacing="1px" cellpadding="1px" tableborder="1px">
    <?php
      $atlanta = array ( "Chama Gaucha" => 40.50, "Aviva by Kameel" => 15.00, "Bone’s Restaurant" => 65.00, "Umi Sushi Buckhead" => 40.50, "Fandangles" => 30.00, "Capital Grille" => 60.50, "Canoe" => 35.50, "One Flew South" => 21.00,"Fox Bros. BBQ" => 15.00, "South City Kitchen Midtown" => 29.00);
-     print "<tr>";   
+     print "<tr><th>Restaurant</th><th>Average Cost</th></tr>";
+     print "<tr>";
      foreach ($atlanta as $key => $val) {
       print "<td>$key</td>";
       print "<td>$val/<td>";    
