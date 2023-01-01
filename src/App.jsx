c2ef8a2be7f0c4b125fe032328cb484b06c9142f
bnonni <bryan@nonni.io>
2023-01-01 12:25:00 -0500

    update projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import ReactLoading from 'react-loading';
 
-import { Introduction, About, Portfolio, Sidebar, Timeline, Blog } from './components';
+import {
+    Introduction,
+    About,
+    Portfolio,
+    Sidebar,
+    Timeline,
+    Blog,
+} from './components';
 
-import { data } from "./data/data";
+import { data } from './data/data';
 import './App.css';
 
 const App = () => {
@@ -14,26 +21,24 @@ const App = () => {
         data ? setLpData(data) : setLoading(true);
     }, []);
 
-    return (
-        loading ? (
-            <div className="loading-container">
-                <ReactLoading className={"loading"} type={"bars"} color={"black"} />
-            </div>
-        ) : (
-            <div id="colorlib-page">
-                <div id="container-wrap">
-                    <Sidebar></Sidebar>
-                    <div style={{ padding: 0 }} id="colorlib-main">
-                        <Introduction></Introduction>
-                        <About></About>
-                        <Portfolio data={lpData.Projects}></Portfolio>
-                        <Blog data={lpData.Posts}></Blog>
-                        <Timeline></Timeline>
-                    </div>
+    return loading ? (
+        <div className="loading-container">
+            <ReactLoading className={'loading'} type={'bars'} color={'black'} />
+        </div>
+    ) : (
+        <div id="colorlib-page">
+            <div id="container-wrap">
+                <Sidebar></Sidebar>
+                <div style={{ padding: 0 }} id="colorlib-main">
+                    <Introduction></Introduction>
+                    <About></About>
+                    <Portfolio data={lpData.Projects}></Portfolio>
+                    <Blog data={lpData.Posts}></Blog>
+                    <Timeline></Timeline>
                 </div>
             </div>
-        )
+        </div>
     );
-}
+};
 
 export default App;
